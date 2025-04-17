2cbcc3855d9232aab7b362131588133c6b994b38
Josue Henrique <josue21henrique@gmail.com>
2025-04-17 11:30:33 -0300

    corrigindo import

--- src/controllers/profileController.ts
+++ src/controllers/profileController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { User } from 'models/User';
+import { User } from '../models/User';
 import { sendNewUserNotificationActive, sendNewUserNotificationInactive } from '../services/mailService';
 
 export const getUsers = async (req: Request, res: Response) => {
